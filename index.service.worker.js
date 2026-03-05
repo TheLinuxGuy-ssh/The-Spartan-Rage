b016c8529b57ee1f05ee9a7a31d660f1828958b7
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-03-05 12:44:56 +0530

    web version update for restructured filemap

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772693123|1339505414';
+const CACHE_VERSION = '1772694873|3089722719';
 /** @type {string} */
 const CACHE_PREFIX = 'God of War:Decon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
