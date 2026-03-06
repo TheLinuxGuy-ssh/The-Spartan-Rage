9c20cffd5d140368929e247cf5e8cb379a71c2f5
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-03-06 21:28:51 +0530

    added proximity detection for objects

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772745237|3299196184';
+const CACHE_VERSION = '1772745279|3341650635';
 /** @type {string} */
 const CACHE_PREFIX = 'God of War:Decon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
