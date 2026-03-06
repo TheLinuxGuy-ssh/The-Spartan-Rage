4bda73696532406b0f3c2b303965a2393dfdcbc0
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-03-06 21:31:31 +0530

    web version update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772745279|3341650635';
+const CACHE_VERSION = '1772812875|23412712578';
 /** @type {string} */
 const CACHE_PREFIX = 'God of War:Decon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
