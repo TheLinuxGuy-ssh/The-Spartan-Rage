9d476c2b522a67ad808687422949e543d7da4593
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-03-07 20:36:48 +0530

    added dynamic health bar (health reduces while attack for testing purposes)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772812875|23412712578';
+const CACHE_VERSION = '1772896003|3437993532';
 /** @type {string} */
 const CACHE_PREFIX = 'God of War:Decon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
